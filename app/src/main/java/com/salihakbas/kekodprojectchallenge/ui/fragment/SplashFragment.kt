5e156aefd537834109c1a8d024eff84c90539238
Salih Akbas <salihhakbass@gmail.com>
2024-08-28 16:15:13 +0300

    Intro anim and fragment created

---
+++ app/src/main/java/com/salihakbas/kekodprojectchallenge/ui/fragment/SplashFragment.kt
@@ -0,0 +1,47 @@
+package com.salihakbas.kekodprojectchallenge.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.salihakbas.kekodprojectchallenge.R
+import com.salihakbas.kekodprojectchallenge.databinding.FragmentSplashBinding
+import com.salihakbas.kekodprojectchallenge.ui.activity.MainActivity
+
+
+class SplashFragment : Fragment() {
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val topAnimation = android.view.animation.AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.top_animation
+        )
+        val bottomAnimation = android.view.animation.AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.bottom_animation
+        )
+        val mainActivity = activity as MainActivity
+        mainActivity.getBottomNavigationView().visibility = View.GONE
+        binding.ivKekod.startAnimation(topAnimation)
+        binding.btnStart.startAnimation(bottomAnimation)
+        binding.tvDesc.startAnimation(bottomAnimation)
+
+        binding.btnStart.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_splashFragment_to_switchFragment)
+        }
+    }
+
+}
