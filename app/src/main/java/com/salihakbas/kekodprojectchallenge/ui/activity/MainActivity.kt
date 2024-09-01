3f2e11aacfb9b054342456443c8d07d3e2f92bdf
Salih Akbas <salihhakbass@gmail.com>
2024-09-01 05:17:18 +0300

    NavigatetoFragment, addMenuItem and removeMenuItem methods deleted. SetBottomNavVisibility method added

--- app/src/main/java/com/salihakbas/kekodprojectchallenge/ui/activity/MainActivity.kt
+++ app/src/main/java/com/salihakbas/kekodprojectchallenge/ui/activity/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.salihakbas.kekodprojectchallenge.ui.activity
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
-import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.salihakbas.kekodprojectchallenge.R
 import com.salihakbas.kekodprojectchallenge.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    lateinit var navHostFragment: NavHostFragment
+    lateinit var bottomNavmenu: Menu
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,69 +25,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        navHostFragment =
+
+        bottomNavmenu = binding.bottomNav.menu
+
+        binding.bottomNav.visibility = View.INVISIBLE
+        val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         NavigationUI.setupWithNavController(binding.bottomNav, navHostFragment.navController)
 
-        binding.bottomNav.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.switchFragment -> {
-                    navigateToFragment(R.id.switchFragment)
-                    true
-                }
 
-                R.id.happinessFragment -> {
-                    navigateToFragment(R.id.happinessFragment)
-                    true
-                }
+    }
 
-                R.id.optimismFragment -> {
-                    navigateToFragment(R.id.optimismFragment)
-                    true
-                }
-
-                R.id.kindnessFragment -> {
-                    navigateToFragment(R.id.kindnessFragment)
-                    true
-                }
-
-                R.id.givingFragment -> {
-                    navigateToFragment(R.id.givingFragment)
-                    true
-                }
-
-                else -> false
-            }
+    fun setBottomNavVisibility(isVisible: Boolean) {
+        if (isVisible) {
+            binding.bottomNav.visibility = View.VISIBLE
+        } else {
+            binding.bottomNav.visibility = View.INVISIBLE
         }
     }
 
-    fun getBottomNavigationView(): BottomNavigationView {
-        return binding.bottomNav
-    }
-
-    fun addMenuItem(title: String, fragmentId: Int, iconResId: Int) {
-        val menu = binding.bottomNav.menu
-        val itemId = fragmentId  // Menü item'ı için ID olarak fragment ID'sini kullanıyoruz
-        val item = menu.findItem(itemId)
-
-        if (item == null) {
-            menu.add(0, itemId, menu.size(), title).setIcon(iconResId).setOnMenuItemClickListener {
-                    navigateToFragment(fragmentId)
-                    true
-                }
-        }
-    }
-
-    fun removeMenuItem(fragmentId: Int) {
-        val menu = binding.bottomNav.menu
-        val item = menu.findItem(fragmentId)
-        item?.let {
-            menu.removeItem(it.itemId)
-        }
-    }
-
-    fun navigateToFragment(fragmentId: Int) {
-        navHostFragment.navController.navigate(fragmentId)
-
-    }
 }
